f8d73e8c5894672c04325ecb11fae6b3ea8c5637
Peter Donald <peter@realityforge.org>
2014-01-23 08:28:49 +1100

    Add tests for isSupported()

--- src/test/java/org/realityforge/gwt/eventsource/client/EventSourceTest.java
+++ src/test/java/org/realityforge/gwt/eventsource/client/EventSourceTest.java
@@ -17,10 +17,13 @@ public class EventSourceTest
   public void registryTest()
   {
     assertNull( EventSource.newEventSourceIfSupported() );
+    assertFalse( EventSource.isSupported() );
     final TestEventSource.Factory factory = new TestEventSource.Factory();
     EventSource.register( factory );
+    assertTrue( EventSource.isSupported() );
     assertNotNull( EventSource.newEventSourceIfSupported() );
     assertTrue( EventSource.deregister( factory ) );
+    assertFalse( EventSource.isSupported() );
     assertNull( EventSource.newEventSourceIfSupported() );
     assertFalse( EventSource.deregister( factory ) );
   }
